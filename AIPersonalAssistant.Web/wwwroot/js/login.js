c9ebf55466c614f0f9d5d208855cbc9b21bb60a8
Camilo Ramirez <juramir@microsoft.com>
2026-01-10 13:05:59 -0800

    Fix default page to load login on startup

--- AIPersonalAssistant.Web/wwwroot/js/login.js
+++ AIPersonalAssistant.Web/wwwroot/js/login.js
@@ -6,5 +6,5 @@ document.getElementById('loginForm').addEventListener('submit', (e) => {
     sessionStorage.setItem('isLoggedIn', 'true');
     sessionStorage.setItem('username', username);
     
-    window.location.href = '/index.html';
+    window.location.href = '/tools.html';
 });
